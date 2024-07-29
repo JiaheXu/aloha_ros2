44b770e5646c66bf290d0b4f44ace79287e5617e
JiaheXu <xjh4438318846@gmail.com>
2024-07-29 10:51:35 -0400

    update processing rgb image part

--- scripts/data_visualization.py
+++ scripts/data_visualization.py
@@ -10,7 +10,7 @@ import numpy as np
 from PIL import Image as im 
 import os
 import argparse
-from PIL import Image
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
@@ -28,54 +28,6 @@ from sensor_msgs.msg import PointCloud2, PointField
 from numpy.linalg import inv
 # from lib_cloud_conversion_between_Open3D_and_ROS import convertCloudFromRosToOpen3d
 from scipy.spatial.transform import Rotation
-
-class Projector:
-    def __init__(self, cloud, label = None) -> None:
-        self.cloud = cloud
-        self.points = np.asarray(cloud.points)
-        self.colors = np.asarray(cloud.colors)
-        self.n = len(self.points)
-        self.label = label
-
-    # intri 3x3, extr 4x4
-    def project_to_rgbd(self,
-                        width,
-                        height,
-                        intrinsic,
-                        extrinsic,
-                        depth_scale,
-                        depth_max
-                        ):
-        depth = 10.0*np.ones((height, width), dtype = float)
-        depth_uint = np.zeros((height, width), dtype=np.uint16)
-        color = np.zeros((height, width, 3), dtype=np.uint8)
-
-        for i in range(0, self.n):
-            point4d = np.append(self.points[i], 1)
-            new_point4d = np.matmul(extrinsic, point4d)
-            point3d = new_point4d[:-1]
-            zc = point3d[2]
-            new_point3d = np.matmul(intrinsic, point3d)
-            new_point3d = new_point3d/new_point3d[2]
-            u = int(round(new_point3d[0]))
-            v = int(round(new_point3d[1]))
-
-            # Fixed u, v checks. u should be checked for width
-            if (u < 0 or u > width - 1 or v < 0 or v > height - 1 or zc <= 0.0 or zc > depth_max):
-                continue
-            if(zc > depth[v][u]):
-                continue
-
-            depth[v][u] = zc
-            depth_uint[v, u] = zc * 1000
-            color[v, u, :] = self.colors[i] * 255
-
-        im_color = o3d.geometry.Image(color)
-        im_depth = o3d.geometry.Image(depth_uint)
-        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
-            im_color, im_depth, depth_scale=1000, depth_trunc=2000, convert_rgb_to_intensity=False)
-        # return rgbd
-        return color,depth, depth_uint, rgbd
 
 def plot_func(src, dst, dir="", idx=0, save = False):
 
@@ -178,31 +130,20 @@ def get_transform( trans, quat):
     # print(t)
     return t
 
-def cropping(xyz, rgb, bound_box, label = None):
+def get_cube_corners( bound_box ):
+    corners = []
+    corners.append( [ bound_box[0][0], bound_box[1][0], bound_box[2][0] ])
+    corners.append( [ bound_box[0][0], bound_box[1][1], bound_box[2][0] ])
+    corners.append( [ bound_box[0][1], bound_box[1][1], bound_box[2][0] ])
+    corners.append( [ bound_box[0][1], bound_box[1][0], bound_box[2][0] ])
 
-    x = xyz[:,0]
-    y = xyz[:,1]
-    z = xyz[:,2]
-    valid_idx = np.where( (x>=bound_box[0][0]) & (x <=bound_box[0][1]) & (y>=bound_box[1][0]) & (y<=bound_box[1][1]) & (z>=bound_box[2][0]) & (z<=bound_box[2][1]) )
-    valid_xyz = xyz[valid_idx]
-    valid_rgb = rgb[valid_idx]
-    valid_label = None
-    if(label is not None):
-        valid_label = label[valid_idx]
-            
-    valid_pcd = o3d.geometry.PointCloud()
-    valid_pcd.points = o3d.utility.Vector3dVector( valid_xyz)
-    if(np.max(valid_rgb) > 1.0):
-        valid_pcd.colors = o3d.utility.Vector3dVector( valid_rgb/255.0 )
-    else:
-        valid_pcd.colors = o3d.utility.Vector3dVector( valid_rgb )
-    return valid_xyz, valid_rgb, valid_label, valid_pcd
+    return corners
 
 def visualize_pcd(pcd, left, right):
     coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
     vis = o3d.visualization.VisualizerWithKeyCallback()
     vis.create_window()
-    coor_frame.scale(0.2, center=coor_frame.get_center())
+    coor_frame.scale(0.1, center=coor_frame.get_center())
     vis.add_geometry(coor_frame)
     vis.get_render_option().background_color = np.asarray([255, 255, 255])
 
@@ -228,107 +169,68 @@ def visualize_pcd(pcd, left, right):
     vis.run()
     vis.destroy_window()
 
-def visualize_bimanual_traj(pcd, left_transforms, right_transforms):
-       
-    coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    vis = o3d.visualization.VisualizerWithKeyCallback()
-    vis.create_window()
-    coor_frame.scale(0.2, center=coor_frame.get_center())
-    vis.add_geometry(coor_frame)
-    vis.get_render_option().background_color = np.asarray([255, 255, 255])
-
-    view_ctl = vis.get_view_control()
-
-    vis.add_geometry(pcd)
-
-    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    object_pcds = [pcd]
-    coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    coor_frame.scale(0.2, center=coor_frame.get_center())
-    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
-
-    for left in left_transforms:
-        left_mesh = copy.deepcopy(mesh).transform(left)
-        left_mesh.scale(0.1, center=left_mesh.get_center())
-        vis.add_geometry(left_mesh)
-
-    for right in right_transforms:
-        right_mesh = copy.deepcopy(mesh).transform(right)
-        right_mesh.scale(0.1, center=right_mesh.get_center())
-        vis.add_geometry(right_mesh)
-    # view_ctl.set_up([-0.4, 0.0, 1.0])
-    # view_ctl.set_front([-4.02516493e-01, 3.62146675e-01, 8.40731978e-01])
-    # view_ctl.set_lookat([0.0 ,0.0 ,0.0])
-    view_ctl.set_up((1, 0, 0))  # set the positive direction of the x-axis as the up direction
-    # view_ctl.set_up((0, -1, 0))  # set the negative direction of the y-axis as the up direction
-    view_ctl.set_front((-0.3, 0.0, 0.2))  # set the positive direction of the x-axis toward you
-    view_ctl.set_lookat((0.0, 0.0, 0.3))  # set the original point as the center point of the window
-    vis.run()
-    vis.destroy_window()
 
 def main():
     
-    data = np.load("./0.npy", allow_pickle = True)
-    
+    data = np.load("./18.npy", allow_pickle = True)
+
     cam_extrinsic = get_transform( [-0.13913296, 0.053, 0.43643044], [-0.63127772, 0.64917582, -0.31329509, 0.28619116])
     o3d_intrinsic = o3d.camera.PinholeCameraIntrinsic(1920, 1080, 734.1779174804688, 734.1779174804688, 993.6226806640625, 551.8895874023438)
 
-    img_size = (256,256)
-    # resized_intrinsic = o3d.camera.PinholeCameraIntrinsic( 256., 25, 80., 734.1779174804688*scale_y, 993.6226806640625*scale_x, 551.8895874023438*scale_y)
-    fxfy = 256.0
-    resized_intrinsic_o3d = o3d.camera.PinholeCameraIntrinsic(256, 256, fxfy, fxfy, 128.0, 128.0)
-    resized_intrinsic_np = np.array([
-        [fxfy, 0., 128.0],
-        [0. ,fxfy,  128.0],
-        [0., 0., 1.0]
-    ])
-
-    bound_box = np.array( [ [0.0, 0.8], [ -0.4 , 0.4], [ -0.2 , 0.4] ] )
-
-
     # for point in data:
-    point = data[0] # only read the first one
-    bgr = point['bgr']
-    rgb = bgr[...,::-1].copy()
-    depth = point['depth']
-    im_color = o3d.geometry.Image(rgb)
-    im_depth = o3d.geometry.Image(depth)
-    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
-        im_color, im_depth, depth_scale=1, depth_trunc=2, convert_rgb_to_intensity=False)
-    original_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
-            rgbd,
-            o3d_intrinsic
-            # resized_intrinsic
-        )
-    original_pcd = original_pcd.transform(cam_extrinsic)
-    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    xyz = np.array(original_pcd.points)
-    rgb = np.array(original_pcd.colors)
-    valid_xyz, valid_rgb, valid_label, cropped_pcd = cropping( xyz, rgb, bound_box )
-    p = Projector(cropped_pcd)
-    rgb, depth, depth_uint8,rgbd = p.project_to_rgbd(256, 256, resized_intrinsic_np, inv(cam_extrinsic), 1000,10)
-    resized_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
-            rgbd,
-            # o3d_intrinsic
-            resized_intrinsic_o3d
-        )
-    resized_pcd.transform( cam_extrinsic )
-    img_data = im.fromarray(rgb)
-    img_data.save('cam_img.png')
+    #     bgr = point['bgr']
+    #     rgb = bgr[...,::-1].copy()
+    #     xyz = point['xyz']/1000.0
+    #     xyz = xyz.reshape(-1,3)
+    #     print("xyz: ", xyz.shape)
 
-    left_transforms = []
-    right_transforms = []
+    #     rgb = rgb.reshape(-1,3)
+    #     rgb = rgb.astype(float)
+    #     bgr = bgr.reshape(-1,3)
+    #     print("rgb: ", rgb.shape)
+    #     pcd = o3d.geometry.PointCloud()
+    #     pcd.points = o3d.utility.Vector3dVector(xyz)
+    #     pcd.colors = o3d.utility.Vector3dVector(rgb/255.0)
+    #     print(pcd)
+    #     visualize_pcd(pcd)
     for point in data:
+        bgr = point['bgr']
+        rgb = bgr[...,::-1].copy()
+        depth = point['depth']
+        # depth = depth.reshape(-1,3)
+        # print("depth: ", depth.shape)
+
+        # rgb = rgb.reshape(-1,3)
+        # rgb = rgb.astype(float)
+        # bgr = bgr.reshape(-1,3)
+        # print("rgb: ", rgb.shape)
+        # pcd = o3d.geometry.PointCloud()
+
+        im_color = o3d.geometry.Image(rgb)
+        im_depth = o3d.geometry.Image(depth)
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            im_color, im_depth, depth_scale=1, depth_trunc=2, convert_rgb_to_intensity=False)
+        
+        final_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+                rgbd,
+                o3d_intrinsic
+            )
+        final_pcd = final_pcd.transform(cam_extrinsic)
+        mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+
         left_transform = get_transform(point['left_ee'][0:3], point['left_ee'][3:7] )
-        right_transform = get_transform(point['right_ee'][0:3], point['right_ee'][3:7] )
+        right_transform = get_transform(point['right_ee'][0:3], point['right_ee'][3:7] ) 
+        # print("left_ee: ",point['left_ee'][0:3])
+        print("right: ",point['right_ee'][0:3])        
+
         left_transform = left_transform @ get_transform( [-0.02, -0.035, -0.045], [0., 0., 0., 1.] )
         right_transform = right_transform @ get_transform( [-0.005, -0.03, -0.036], [0., 0., 0., 1.] )
+        visualize_pcd(final_pcd, left_transform, right_transform)
+        # print("left: ", )
+        # print("right: ", inv(get_transform(point['right_ee'][0:3], point['right_ee'][3:7] ) ))
 
-        left_transforms.append(left_transform)
-        right_transforms.append(right_transform)
-    # print("len: ", left_transforms)
-    visualize_bimanual_traj(resized_pcd, left_transforms, right_transforms)
-     
+        
+        # print("right: ", point['right_ee'])
 
 if __name__ == "__main__":
     main()
