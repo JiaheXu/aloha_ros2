63125abc11b5061c757f2320faa2cc705e8b00af
JiaheXu <xjh4438318846@gmail.com>
2024-07-31 20:22:58 -0400

    update

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -50,8 +50,8 @@ class Projector:
         depth = 10.0*np.ones((height, width), dtype = float)
         depth_uint = np.zeros((height, width), dtype=np.uint16)
         color = np.zeros((height, width, 3), dtype=np.uint8)
-        xyz =  np.full((height, width, 3), np.nan)
-
+        # xyz =  np.full((height, width, 3), np.nan)
+        xyz =  np.zeros((height, width, 3), dtype = float)
 
         for i in range(0, self.n):
             point4d = np.append(self.points[i], 1)
@@ -79,21 +79,86 @@ class Projector:
         rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
             im_color, im_depth, depth_scale=1000, depth_trunc=2000, convert_rgb_to_intensity=False)
         # return rgbd
-        return color, depth, xyz, rgbd
+        return color, depth, xyz, rgbd, depth_uint
 
+# def get_all_valid_depth( depth ):
+#     for x in range( depth.shape[0] ):
+#         for y in range(depth.shape[1]):
+#             if( np.isnan(depth[x][y]) == False ): # valid
+#                 if( y + 1 < depth.shape[1] ):
+#                     if( np.isnan(depth[x][y+1] ) ):
+#                         depth[x][y+1] = depth[x][y]
 
-def get_init_trans(src, dst):
-    if(src.shape[0] == 3):
-        src = np.transpose(src)
-    if(dst.shape[0] == 3):
-        dst = np.transpose(dst)
-    trans = np.mean(src, axis = 0) - np.mean(dst, axis = 0)
-    trans = trans.reshape(3,1)
-    return trans
+#     for x in range( depth.shape[0] ):
+#         for y in reversed( range(depth.shape[1]) ):
+#             if( np.isnan(depth[x][y]) == False ): # valid
+#                 if( y -1 >= 0 ):
+#                     if( np.isnan(depth[x][y-1] ) ):
+#                         depth[x][y-1] = depth[x][y]
 
+#     for y in range( depth.shape[1] ):
+#         for x in range(depth.shape[0] ):
+#             if( np.isnan(depth[x][y]) == False ): # valid
+#                 if( x + 1 < depth.shape[0] ):
+#                     if( np.isnan(depth[x+1][y] ) ):
+#                         depth[x+1][y] = depth[x][y]
 
+#     for y in range( depth.shape[1] ):
+#         for x in reversed( range(depth.shape[0] ) ):
+#             if( np.isnan(depth[x][y]) == False ): # valid
+#                 if( x - 1 >= 0 ):
+#                     if( np.isnan(depth[x-1][y] ) ):
+#                         depth[x-1][y] = depth[x][y]
+#     invalid_idx = np.where( np.isnan(depth) )[0]
+#     depth[invalid_idx] = 10.0
+#     return depth
+def get_all_valid_depth( depth , xyz):
+    for x in range( depth.shape[0] ):
+        for y in range(depth.shape[1]):
+            if( depth[x][y] > 0 ): # valid
+                if( y + 1 < depth.shape[1] ):
+                    if( depth[x][y+1] == 0 ):
+                        depth[x][y+1] = depth[x][y]
+                        xyz[x][y+1] = xyz[x][y]
 
-    
+    for x in range( depth.shape[0] ):
+        for y in reversed( range(depth.shape[1]) ):
+            if( depth[x][y] > 0 ): # valid
+                if( y -1 >= 0 ):
+                    if( depth[x][y-1]==0 ):
+                        depth[x][y-1] = depth[x][y]
+                        xyz[x][y-1] = xyz[x][y]
+
+    for y in range( depth.shape[1] ):
+        for x in range(depth.shape[0] ):
+            if( depth[x][y] > 0 ): # valid
+                if( x + 1 < depth.shape[0] ):
+                    if( depth[x+1][y]==0 ):
+                        depth[x+1][y] = depth[x][y]
+                        xyz[x+1][y] = xyz[x][y]
+
+    for y in range( depth.shape[1] ):
+        for x in reversed( range(depth.shape[0] ) ):
+            if( depth[x][y] > 0 ): # valid
+                if( x - 1 >= 0 ):
+                    if( depth[x-1][y]==0 ):
+                        depth[x-1][y] = depth[x][y]
+                        xyz[x-1][y] = xyz[x][y]
+    return depth, xyz
+
+def get_xyz_from_depth(depth, intrinsic, cam_extrinsic):
+    xyz = np.zeros( (*depth.shape,3))
+    for i in range(depth.shape[0]):
+        for j in range(depth.shape[1]):
+            z = depth[i][j]
+            x = (i - intrinsic[0][2] ) * z / intrinsic[0][0]
+            y = (j - intrinsic[1][2] ) * z / intrinsic[1][1]
+            point = np.array([x,y,z,1.])
+            # point = point.reshape(4,)
+            world_coord = cam_extrinsic @ point
+            xyz = world_coord[0:3]
+    return xyz
+
 def display_inlier_outlier(cloud, ind):
     inlier_cloud = cloud.select_by_index(ind)
     outlier_cloud = cloud.select_by_index(ind, invert=True)
@@ -118,38 +183,6 @@ def draw_registration_result(source, target, transformation):
     target_temp.paint_uniform_color([0, 0.651, 0.929])
     source_temp.transform(transformation)
     o3d.visualization.draw_geometries([source_temp, target_temp])
-
-def colored_ICP(source, target):
-    voxel_radius = [0.002, 0.002, 0.002]
-    max_iter = [50, 30, 14]
-    current_transformation = np.identity(4)
-    print("3. Colored point cloud registration")
-    for scale in range(3):
-        iter = max_iter[scale]
-        radius = voxel_radius[scale]
-        print([iter, radius, scale])
-
-        print("3-1. Downsample with a voxel size %.2f" % radius)
-        source_down = source.voxel_down_sample(radius)
-        target_down = target.voxel_down_sample(radius)
-
-        print("3-2. Estimate normal.")
-        source_down.estimate_normals(
-            o3d.geometry.KDTreeSearchParamHybrid(radius=radius * 2, max_nn=30))
-        target_down.estimate_normals(
-            o3d.geometry.KDTreeSearchParamHybrid(radius=radius * 2, max_nn=30))
-
-        print("3-3. Applying colored point cloud registration")
-        result_icp = o3d.pipelines.registration.registration_colored_icp(
-            source_down, target_down, radius, current_transformation,
-            o3d.pipelines.registration.TransformationEstimationForColoredICP(),
-            o3d.pipelines.registration.ICPConvergenceCriteria(relative_fitness=1e-6,
-                                                            relative_rmse=1e-6,
-                                                            max_iteration=iter))
-        current_transformation = result_icp.transformation
-        print(result_icp)
-    draw_registration_result_original_color(source, target,
-                                            result_icp.transformation)
 
 
 def get_transform( trans, quat):
@@ -245,11 +278,73 @@ def visualize_bimanual_traj(pcd, left_transforms, right_transforms):
         right_mesh = copy.deepcopy(mesh).transform(right)
         right_mesh.scale(0.1, center=right_mesh.get_center())
         vis.add_geometry(right_mesh)
-    # view_ctl.set_up([-0.4, 0.0, 1.0])
-    # view_ctl.set_front([-4.02516493e-01, 3.62146675e-01, 8.40731978e-01])
-    # view_ctl.set_lookat([0.0 ,0.0 ,0.0])
+
     view_ctl.set_up((1, 0, 0))  # set the positive direction of the x-axis as the up direction
-    # view_ctl.set_up((0, -1, 0))  # set the negative direction of the y-axis as the up direction
+    view_ctl.set_front((-0.3, 0.0, 0.2))  # set the positive direction of the x-axis toward you
+    view_ctl.set_lookat((0.0, 0.0, 0.3))  # set the original point as the center point of the window
+    vis.run()
+    vis.destroy_window()
+
+def visualize_pcd_transform(pcd, transforms):
+
+    coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
+    coor_frame.scale(0.2, center=coor_frame.get_center())
+    vis.add_geometry(coor_frame)
+    vis.get_render_option().background_color = np.asarray([255, 255, 255])
+
+    view_ctl = vis.get_view_control()
+
+    vis.add_geometry(pcd)
+
+    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    object_pcds = [pcd]
+    coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    coor_frame.scale(0.2, center=coor_frame.get_center())
+    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+
+    for right in transforms:
+        right_mesh = copy.deepcopy(mesh).transform(get_transform(right[0:3], right[3:7]) )
+        right_mesh.scale(0.1, center=right_mesh.get_center())
+        vis.add_geometry(right_mesh)
+
+    view_ctl.set_up((1, 0, 0))  # set the positive direction of the x-axis as the up direction
+    view_ctl.set_front((-0.3, 0.0, 0.2))  # set the positive direction of the x-axis toward you
+    view_ctl.set_lookat((0.0, 0.0, 0.3))  # set the original point as the center point of the window
+    vis.run()
+    vis.destroy_window()
+
+def visualize_pcd_delta_transform(pcd, start_t, delta_transforms):
+
+    coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
+    coor_frame.scale(0.2, center=coor_frame.get_center())
+    vis.add_geometry(coor_frame)
+    vis.get_render_option().background_color = np.asarray([255, 255, 255])
+
+    view_ctl = vis.get_view_control()
+
+    vis.add_geometry(pcd)
+
+    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    object_pcds = [pcd]
+    coor_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    coor_frame.scale(0.2, center=coor_frame.get_center())
+    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+
+    mesh = mesh.transform( get_transform(start_t[0:3], start_t[3:7]) )
+    new_mesh = copy.deepcopy(mesh)
+    new_mesh.scale(0.1, center=new_mesh.get_center())
+    vis.add_geometry(new_mesh)
+    for delta_t in delta_transforms:
+        mesh = mesh.transform( get_transform(delta_t[0:3], delta_t[3:7]))
+        new_mesh = copy.deepcopy(mesh)
+        new_mesh.scale(0.1, center=new_mesh.get_center())
+        vis.add_geometry(new_mesh)
+
+    view_ctl.set_up((1, 0, 0))  # set the positive direction of the x-axis as the up direction
     view_ctl.set_front((-0.3, 0.0, 0.2))  # set the positive direction of the x-axis toward you
     view_ctl.set_lookat((0.0, 0.0, 0.3))  # set the original point as the center point of the window
     vis.run()
@@ -307,6 +402,7 @@ def main():
                     # resized_intrinsic
                 )
             original_pcd = original_pcd.transform(cam_extrinsic)
+
             mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
             xyz = np.array(original_pcd.points)
             rgb = np.array(original_pcd.colors)
@@ -314,27 +410,42 @@ def main():
             
             p = Projector(cropped_pcd)
 
-            rgb, depth, xyz, rgbd = p.project_to_rgbd(256, 256, resized_intrinsic_np, inv(cam_extrinsic), 1000,10)
-            resized_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+            rgb, depth, xyz, rgbd, depth_uint = p.project_to_rgbd(256, 256, resized_intrinsic_np, inv(cam_extrinsic), 1000,10)
+
+            # print("original min max ", np.min(depth), np.max(depth) )
+
+            # verify all valid depth
+            # all_valid_depth_uint, xyz = get_all_valid_depth( depth_uint, xyz )
+            # all_valid_depth = ( all_valid_depth_uint.astype(float) ) / 1000.0
+            # im_color = o3d.geometry.Image(rgb)
+            # im_depth = o3d.geometry.Image(all_valid_depth_uint)
+            # rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            #     im_color, im_depth, depth_scale=1000, depth_trunc=2000, convert_rgb_to_intensity=False)
+
+            all_valid_resized_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
                     rgbd,
-                    # o3d_intrinsic
                     resized_intrinsic_o3d,
-                    # remove_nan_points=False
+
                 )
-            # print(resized_pcd)
-            resized_pcd.transform( cam_extrinsic )
-            resized_img_data = im.fromarray(rgb)
-            resized_img_data.save('cam_img.png')
+            all_valid_resized_pcd.transform( cam_extrinsic )
+            # o3d.visualization.draw_geometries([all_valid_resized_pcd])
 
             # verify xyz and depth value
             # valid_pcd = o3d.geometry.PointCloud()
             # xyz = xyz.reshape(-1,3)
             # rgb = (rgb/255.0).reshape(-1,3)
-            # x = xyz[:,0]
-            # valid_mask = np.where( np.logical_not(np.isnan(x) ) )
-            # valid_pcd.points = o3d.utility.Vector3dVector( xyz[valid_mask] )
-            # valid_pcd.colors = o3d.utility.Vector3dVector( rgb[valid_mask] )
+            # valid_pcd.points = o3d.utility.Vector3dVector( xyz )
+            # valid_pcd.colors = o3d.utility.Vector3dVector( rgb )
             # o3d.visualization.draw_geometries([valid_pcd])
+            if( len( np.where( np.isnan(xyz))[0] ) >0 ):
+                print(np.where( np.isnan(xyz)))
+                print(" x y z has invalid point !!!!!")
+                print(" x y z has invalid point !!!!!")
+                print(" x y z has invalid point !!!!!")
+                raise
+
+            # print("invalid points: ", np.where( np.isnan(xyz)))
+            ########################################################################################## save data
 
             resized_img_data = np.transpose(rgb, (2, 0, 1) ).astype(float)
             # print("resized_img_data: ", resized_img_data.shape)
@@ -368,18 +479,18 @@ def main():
             delta_right_trajectory = []
 
             for point in data:
-                left_gripper_max = max(left_gripper_max, point["left_pos"][6])
-                left_gripper_min = min(left_gripper_min, point["left_pos"][6])
+                # left_gripper_max = max(left_gripper_max, point["left_pos"][6])
+                # left_gripper_min = min(left_gripper_min, point["left_pos"][6])
                 right_gripper_max = max(right_gripper_max, point["right_pos"][6])
                 right_gripper_min = min(right_gripper_min, point["right_pos"][6])
 
             for point in data:
-                left_transform = get_transform(point['left_ee'][0:3], point['left_ee'][3:7] )
-                left_transform = left_transform @ get_transform( [-0.02, -0.035, -0.045], [0., 0., 0., 1.] )
-                left_rot = Rotation.from_matrix(left_transform[:3,:3])
-                left_quat = left_rot.as_quat()
-                left_openess = ( float(point["left_pos"][6]) - left_gripper_min ) / (left_gripper_max - left_gripper_min )
-                left_trajectory.append(np.array( [left_transform[0][3], left_transform[1][3], left_transform[2][3], left_quat[0], left_quat[1], left_quat[2], left_quat[3], left_openess ] ))
+                # left_transform = get_transform(point['left_ee'][0:3], point['left_ee'][3:7] )
+                # left_transform = left_transform @ get_transform( [-0.02, -0.035, -0.045], [0., 0., 0., 1.] )
+                # left_rot = Rotation.from_matrix(left_transform[:3,:3])
+                # left_quat = left_rot.as_quat()
+                # left_openess = ( float(point["left_pos"][6]) - left_gripper_min ) / (left_gripper_max - left_gripper_min )
+                # left_trajectory.append(np.array( [left_transform[0][3], left_transform[1][3], left_transform[2][3], left_quat[0], left_quat[1], left_quat[2], left_quat[3], left_openess ] ))
 
                 right_transform = get_transform(point['right_ee'][0:3], point['right_ee'][3:7] )
                 right_transform = right_transform @ get_transform( [-0.005, -0.03, -0.036], [0., 0., 0., 1.] )
@@ -388,22 +499,38 @@ def main():
                 right_openess = ( float(point["right_pos"][6]) - right_gripper_min ) / (right_gripper_max - right_gripper_min )
                 right_trajectory.append(np.array( [right_transform[0][3], right_transform[1][3], right_transform[2][3], right_quat[0], right_quat[1], right_quat[2], right_quat[3], right_openess] ))  
                 
+                # print("right_openess: ", right_openess)
+            right_trajectory_np = np.array(right_trajectory)
+            max_x = np.max( right_trajectory_np[:,0])
+            max_y = np.max( right_trajectory_np[:,1])
+            max_z = np.max( right_trajectory_np[:,2])
+            min_x = np.min( right_trajectory_np[:,0])
+            min_y = np.min( right_trajectory_np[:,1])
+            min_z = np.min( right_trajectory_np[:,2])
+            # print("max: ", max_x, max_y, max_z)
+            # print("min: ", min_x, min_y, min_z)
+
             for idx, trans in enumerate(right_trajectory, 0):
                 if(idx == 0):
                     continue
                 delta_trans = get_transform(right_trajectory[idx][0:3], right_trajectory[idx][3:7]) @ inv( get_transform(right_trajectory[idx-1][0:3], right_trajectory[idx-1][3:7] ) )
                 delat_rot = Rotation.from_matrix(delta_trans[:3,:3])
                 delta_quat = delat_rot.as_quat()
-                delta_openess = right_trajectory[idx][-1] - right_trajectory[idx-1][-1]
-                action = np.array( [delta_trans[0,3], delta_trans[1,3], delta_trans[2,3], delta_quat[0], delta_quat[1], delta_quat[2], delta_quat[3], delta_openess] )
+                openess = right_trajectory[idx][-1]
+                # print("delta_openess: ", delta_openess)
+                action = np.array( [delta_trans[0][3], delta_trans[1][3], delta_trans[2][3], delta_quat[0], delta_quat[1], delta_quat[2], delta_quat[3], openess] )
                 delta_right_trajectory.append( action )
+
+            # visualize_pcd_transform(all_valid_resized_pcd, right_trajectory)
+            # visualize_pcd_delta_transform(all_valid_resized_pcd, right_trajectory[0], delta_right_trajectory)
+
 
             delta_transform = get_transform(right_trajectory[-1][0:3], right_trajectory[-1][3:7]) @ inv( get_transform(right_trajectory[0][0:3], right_trajectory[0][3:7] ) )
             delat_rot = Rotation.from_matrix(delta_transform[:3,:3])
             delta_quat = delat_rot.as_quat()
-            delta_openess = right_trajectory[-1][-1] - right_trajectory[0][-1]
+            delta_openess = right_trajectory[-1][-1]
             
-            action = np.array( [delta_transform[0,3], delta_transform[1,3], delta_transform[2,3], delta_quat[0], delta_quat[1], delta_quat[2], delta_quat[3], delta_openess] )
+            action = np.array( [delta_transform[0][3], delta_transform[1][3], delta_transform[2][3], delta_quat[0], delta_quat[1], delta_quat[2], delta_quat[3], delta_openess] )
             action = action.reshape(1,8)
 
             gripper = copy.deepcopy( right_trajectory[0])
