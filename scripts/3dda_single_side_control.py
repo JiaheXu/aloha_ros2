5b62784d7bf5d6889c9efd83b222d37235d528a4
JiaheXu <xjh4438318846@gmail.com>
2024-08-10 20:22:56 -0400

    update control script

--- scripts/3dda_single_side_control.py
+++ scripts/3dda_single_side_control.py
@@ -50,14 +50,71 @@ def un_normalize_gripper( position , min_joint = 0.6, max_joint = 1.6):
     joint_pos = position * (max_joint - min_joint) + min_joint
 
     return joint_pos
+
+def opening_ceremony(
+
+    follower_bot_right: InterbotixManipulatorXS,
+) -> None:
+    """Move all 4 robots to a pose where it is easy to start demonstration."""
+    # reboot gripper motors, and set operating modes for all motors
+
+
+    follower_bot_right.core.robot_reboot_motors('single', 'gripper', True)
+    follower_bot_right.core.robot_set_operating_modes('group', 'arm', 'position')
+    follower_bot_right.core.robot_set_operating_modes(
+        'single', 'gripper', 'current_based_position'
+    )
+
+    follower_bot_right.core.robot_set_motor_registers('single', 'gripper', 'current_limit', 300)
+
+    # torque_on(follower_bot_left)
+    # torque_on(leader_bot_left)
+    torque_on(follower_bot_right)
+    # torque_on(leader_bot_right)
+
+    # move arms to starting position
+    start_arm_qpos = START_ARM_POSE[:6]
+    move_arms(
+        [follower_bot_right],
+        [start_arm_qpos] * 4,
+        moving_time=4.0,
+    )
+    # move grippers to starting position
+    move_grippers(
+        [ follower_bot_right],
+        [LEADER_GRIPPER_JOINT_MID, FOLLOWER_GRIPPER_JOINT_CLOSE] * 2,
+        moving_time=0.5
+    )
+
+def press_to_start(
+    leader_bot_left: InterbotixManipulatorXS,
+    leader_bot_right: InterbotixManipulatorXS,
+) -> None:
+    # press gripper to start teleop
+    # disable torque for only gripper joint of leader robot to allow user movement
+    leader_bot_left.core.robot_torque_enable('single', 'gripper', False)
+    leader_bot_right.core.robot_torque_enable('single', 'gripper', False)
+    print('Close the grippers to start')
+    pressed = False
+    while rclpy.ok() and not pressed:
+        pressed = (
+            (get_arm_gripper_positions(leader_bot_left) < LEADER_GRIPPER_CLOSE_THRESH) and
+            (get_arm_gripper_positions(leader_bot_right) < LEADER_GRIPPER_CLOSE_THRESH)
+        )
+        get_interbotix_global_node().get_clock().sleep_for(DT_DURATION)
+    torque_off(leader_bot_left)
+    torque_off(leader_bot_right)
+    print('Started!')
+
+
 def main() -> None:
     node = create_interbotix_global_node('aloha')
-    follower_bot_left = InterbotixManipulatorXS(
-        robot_model='vx300s',
-        robot_name='follower_left',
-        node=node,
-        iterative_update_fk=False,
-    )
+    # follower_bot_left = InterbotixManipulatorXS(
+    #     robot_model='vx300s',
+    #     robot_name='follower_left',
+    #     node=node,
+    #     iterative_update_fk=False,
+    # )
     follower_bot_right = InterbotixManipulatorXS(
         robot_model='vx300s',
         robot_name='follower_right',
@@ -67,6 +124,13 @@ def main() -> None:
 
     robot_startup(node)
 
+    opening_ceremony(
+
+        follower_bot_right,
+    )
+
+    # press_to_start(leader_bot_left, leader_bot_right)
+
     # Teleoperation loop
     # gripper_left_command = JointSingleCommand(name='gripper')
     gripper_right_command = JointSingleCommand(name='gripper')
@@ -75,11 +139,14 @@ def main() -> None:
     action = np.load("ep2_action.npy", allow_pickle = True)
     action = action.reshape(-1,8)
     length = action.shape[0]
+    
+    length = 1
+    test = np.array([0.4, 0., 0.4, 0., 0., 0., 1.,0.] ) 
     # while rclpy.ok():
     idx = 0
     while( idx < length):
-        T_sd = get_transform( action[idx][0:7])
-        right_gripper = un_normalize_gripper( action[idx][7] )
+        T_sd = get_transform( test[0:7])
+        right_gripper = un_normalize_gripper( test[7] )
         gripper_right_command.cmd = LEADER2FOLLOWER_JOINT_FN(
             right_gripper
         )
@@ -91,7 +158,7 @@ def main() -> None:
         follower_bot_right.gripper.core.pub_single.publish(gripper_right_command)
         # sleep DT
         get_interbotix_global_node().get_clock().sleep_for(DT_DURATION)
-
+    print("finished !!!!!!!!!!" )
     robot_shutdown(node)
 
 
